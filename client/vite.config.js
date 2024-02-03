b34f6a02feddbaa0082c73aea774b01d866a6216
Tedy Francy <tedy@autosmarts.co>
2024-02-04 00:52:22 +0530

    complete sign up page functionality

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { FaLessThanEqual } from 'react-icons/fa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server : {
+    proxy : {
+      '/api' : {
+        target : 'http://localhost:3000',
+        secure : false
+      },
+
+    }
+  },
   plugins: [react()],
 })
