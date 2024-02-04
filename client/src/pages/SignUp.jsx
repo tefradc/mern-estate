7683a7f12ce5f05c2adbabdc6f0434a98897431f
Tedy Francy <tedy@autosmarts.co>
2024-02-04 13:05:51 +0530

    complete sign in page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -48,8 +48,9 @@ export default function SignUp() {
      </form>
      <div className='flex gap-2 mt-5'>
       <p>Have an account ?</p>
-      <Link to={"/sign-in"}></Link>
+      <Link to={"/sign-in"}>
       <span className='text-blue-700'>Sign in</span>
+      </Link>
      </div>
     </div>
   )
